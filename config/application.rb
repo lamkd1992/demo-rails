33332a25fcf4bf113f1e5656c4a9c3364c761803
Kim Lam <lamkd1992@gmail.com>
2021-03-26 14:42:13 +0700

    Update config enviroments

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # config.generators do |g|
+    #   g.assets false
+    #   g.helper false
+    #   g.template_engine false
+    # end
   end
 end
